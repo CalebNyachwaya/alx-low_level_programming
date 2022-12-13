49e64b9432892a8565c4701d6521dab00ea6460f
CalebNyachwaya <caleb.nyachwaya@gmail.com>
2022-12-13 13:17:02 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include
+
+/**
+ * main - entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+
+	for (l = 'A'; l <= 'Z'; l++)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
