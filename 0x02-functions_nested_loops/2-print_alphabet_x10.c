e1197d79b6492b15bdc01daec5b6d769a9b20ff1
CalebNyachwaya <caleb.nyachwaya@gmail.com>
2022-12-14 14:57:58 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char az;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (az = 'a'; az <= 'z'; az++)
+		{
+			_putchar(az);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
