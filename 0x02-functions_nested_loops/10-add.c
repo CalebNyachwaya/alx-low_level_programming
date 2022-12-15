192187243386e3315234c444be2dd249ddc77645
CalebNyachwaya <caleb.nyachwaya@gmail.com>
2022-12-15 15:19:38 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * @cal: value to be added
+ * @lili: valeue to be added
+ *
+ * Return: Always sum
+ */
+
+int add(int cal, int lili);
+{
+	int sum = cal + lili;
+
+	return (sum);
+}
