bf229333a42604ed348b117606e34abf55b9ff48
CalebNyachwaya <caleb.nyachwaya@gmail.com>
2022-12-14 16:01:24 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,20 +4,18 @@
 /**
  * print_alphabet - Entry Point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 void print_alphabet(void)
 {
+int count = 0;
 	char az;
-	int i;
 
-	for (i = 1; i <= 10; i++)
+	while (count++ <= 9)
 	{
 		for (az = 'a'; az <= 'z'; az++)
-		{
 			_putchar(az);
-		}
 		_putchar('\n');
 	}
 }
