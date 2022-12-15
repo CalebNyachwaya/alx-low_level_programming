d4892ead5d6fce6932ffa07f10d5df08e4899fa4
CalebNyachwaya <caleb.nyachwaya@gmail.com>
2022-12-15 02:32:32 -0800

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,14 @@
 
 /**
  * _islower - Entry point
+ * @c: we are checking the character
  *
  * Return: 1 if c is lowercase
  */
 
 int _islower(int c)
 {
+
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
