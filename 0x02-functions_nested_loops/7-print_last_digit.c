dff19c795bff68433d55caeae2551e5f1a4efd89
CalebNyachwaya <caleb.nyachwaya@gmail.com>
2022-12-15 11:11:43 -0800

    7. There are only 3 colors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,12 @@
 
 int print_last_digit(int n)
 {
-	int last = n % 10;
-	int t = last * -1;
+		int last = n % 10;
 
 	if (last < 0)
-	putchar(t  + '0');
-	else
-	putchar(last + '0');
+		last *= -1;
+
+	_putchar(last + '0');
+
+	return (last);
 }
