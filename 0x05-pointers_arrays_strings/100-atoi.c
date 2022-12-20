31377beb6af128d163534bc1eaa6e2e0731458e4
CalebNyachwaya <caleb.nyachwaya@gmail.com>
2022-12-20 11:45:03 -0800

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,8 +4,22 @@
 *
 * Return: integer
 */
-
 int _atoi(char *s)
 {
-	return (*s);
+	int sign = 1;
+	unsigned int n = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+
+		else if (n > 0)
+			break;
+
+	} while (*s++);
+
+	return (n * sign);
 }
