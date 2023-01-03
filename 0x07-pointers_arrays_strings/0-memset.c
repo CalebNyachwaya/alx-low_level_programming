1373fe87a0fb410bc21d2d9f415128e4c7f51e56
CalebNyachwaya <caleb.nyachwaya@gmail.com>
2023-01-03 07:18:03 -0800

    perform same role as memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,8 +4,8 @@
 /**
  * _memset - Fills the first n bytes of the memory area
  * pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
+ * @s: Direction
+ * @b: The constant character
  * @n: The number of bytes to be filled.
  * Return: s.
  */
