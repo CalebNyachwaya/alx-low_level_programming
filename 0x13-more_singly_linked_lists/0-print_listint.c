044872f72b5cbb8ba634269db284e7c76b13ce7d
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-02-02 05:55:36 -0800

    replace putchar with printf

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -17,8 +17,11 @@ size_t print_listint(const listint_t *h)
 	while (h)
 	{
 		elements++;
-		_putchar((h->n) + '0');
-		_putchar('\n');
+		/*
+		 * _putchar((h->n) + '0');
+		 * _putchar('\n');
+		 */
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (elements);
