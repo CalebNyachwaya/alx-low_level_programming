311d352e88c788b5ac7604c4d3a935a43520a30d
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-04-10 10:02:00 -0700

    fix Betty style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,8 +5,8 @@
  *
  * @key: char key of the values
  * @size: size of the array of the hash table
- * Return: the index at which the key/value pair should be stored 
- */ 
+ * Return: the index at which the key/value pair should be stored
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_index;
